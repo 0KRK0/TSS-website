0633be7fd358ab7da875170615f6ed2f2a30489d
Kona Rajesh kumar <20BQ1A4723@vvit.net>
2023-07-06 16:14:01 +0530

    day-1

---
+++ client/src/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div id="home-page-socialmedia-logo-rules">
+            <div>
+                {/* <!-- Links  --> */}
+            </div>
+            <div>
+                 {/* <!-- logo  --> */}
+            </div>
+            <div>
+                {/* <!-- Rules ad=nd privacy policies and privacy cookies  --> */}
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
