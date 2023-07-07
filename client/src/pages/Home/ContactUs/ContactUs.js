518892005bc829309dcc121219b5e0de928ea2c4
Anjani sai sudheer divyakolu <sudheerdivyakolu@gmail.com>
2023-07-07 12:16:50 +0530

    added

---
+++ client/src/pages/Home/ContactUs/ContactUs.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <div id="home-page-contactus">
+        
+        <div id="home-page-location-contact">
+            <div id="home-page-location">
+                {/* <!-- Location  --> */}
+            </div>
+            <div id="home-page-contact">
+                <div id="home-page-contact-data">
+                    <label>Email</label>
+                    <input />
+                    <label>Ask a Question ?</label>
+                    <input />
+                    <button >Submit</button>
+                </div>
+                <div id="home-page-thanking">
+                    {/* <!-- THANK you --> */}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ContactUs
---
+++ client/src/pages/Home/Services.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+<<<<<<< HEAD
+const Service = () => {
+  return (
+    <div id="home-page-services">
+        <div id="home-page-service-article-1">
+            <h3>Service-1</h3>
+            <article id="home-page-service-article"></article>
+            <img src="" alt=''/>
+            <button>Read more</button>
+        </div>
+        <div id="home-page-service-article-2">
+            <h3>Service-2</h3>
+            <article id="home-page-service-article"></article>
+            <img src="" alt=''/>
+            <button>Read more</button>
+        </div>
+        <div id="home-page-service-article-3">
+            <h3>Service-3</h3>
+            <article id="home-page-service-article"></article>
+            <img src="" alt=''/>
+            <button>Read more</button>
+        </div>
+=======
+const Services = () => {
+  return (
+    <div id="home-page-services">
+      <div id="home-page-service-article-1">
+        <h3>Service-1</h3>
+        <article id="home-page-service-article"></article>
+        <img src="" alt='' />
+        <button>Read more</button>
+      </div>
+      <div id="home-page-service-article-2">
+        <h3>Service-2</h3>
+        <article id="home-page-service-article"></article>
+        <img src="" alt='' />
+        <button>Read more</button>
+      </div>
+      <div id="home-page-service-article-3">
+        <h3>Service-3</h3>
+        <article id="home-page-service-article"></article>
+        <img src="" alt='' />
+        <button>Read more</button>
+      </div>
+>>>>>>> edf3947b69dba51bfe17e46f294ff0c7f9ec2da5
+    </div>
+  )
+}
+
+<<<<<<< HEAD
+export default Service
+=======
+export default Services
+>>>>>>> edf3947b69dba51bfe17e46f294ff0c7f9ec2da5
