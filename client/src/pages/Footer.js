bd93505c059e442fbc3632f3eb2d860c35efc500
Anjani sai sudheer divyakolu <sudheerdivyakolu@gmail.com>
2023-07-07 11:28:10 +0530

    footer

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -5,10 +5,17 @@ const Footer = () => {
     <footer>
         <div id="home-page-socialmedia-logo-rules">
             <div>
-                {/* <!-- Links  --> */}
+              <nav>
+                <ui>
+                  <a href ="/about"><li>About Us</li></a>
+                  <a href ="/services"><li>Services</li></a>
+                  <a href ="/careers"><li>Careers</li></a>
+                  <a href ="#"><li>Contact info</li></a>
+                </ui>
+              </nav>   
             </div>
             <div>
-                 {/* <!-- logo  --> */}
+              <img src="logo2.1.jpg"  alt="Tech Stalwart Solution"/> 
             </div>
             <div>
                 {/* <!-- Rules ad=nd privacy policies and privacy cookies  --> */}
