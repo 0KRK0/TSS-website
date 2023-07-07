fe82b5901a27ea57bb7ad740c48dab08777a1200
Anjani sai sudheer divyakolu <sudheerdivyakolu@gmail.com>
2023-07-07 12:18:45 +0530

    thrree

---
+++ client/src/pages/Home/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../../static_files/Header.css';
+
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            <ui>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Services</li>
+                <li>Careers</li>
+                <li>Contact Us</li>  
+            </ui>
+            <img src="logo2.1.jpg"  alt="Tech Stalwart Solution"/>
+        </nav>
+        <div>
+            {/* <!-- About the nav  on HOVER --> */}
+        </div>
+    </header>
+  )
+}
+
+export default Header
