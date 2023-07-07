edf3947b69dba51bfe17e46f294ff0c7f9ec2da5
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-07-07 12:07:23 +0530

    contactus created

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,21 +1,21 @@
 import '../static_files/App.css';
+import Footer from './Home/Footer';
 import Header from './Home/Header';
 import Carousel from './Home/Carousel';
-import Career from './Home/Career';
+import Careers from './Home/Careers';
 import Insight from './Home/Insight';
-import Service from './Home/Services';
-import ContactUs from './Home/ContactUs';
-import Footer from './Home/Footer';
+import Services from './Home/Services';
+import Form from './Home/ContactUs/ContactUs';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Carousel />
-      <Career />
+      <Careers />
       <Insight />
-      <Service />
-      <ContactUs />
+      <Services />
+      <Form />
       <Footer />
     </div>
   );
--- client/src/pages/Home/Services.js
+++ client/src/pages/Home/Services.js
@@ -1,28 +1,28 @@
 import React from 'react'
 
-const Service = () => {
+const Services = () => {
   return (
     <div id="home-page-services">
-        <div id="home-page-service-article-1">
-            <h3>Service-1</h3>
-            <article id="home-page-service-article"></article>
-            <img src="" alt=''/>
-            <button>Read more</button>
-        </div>
-        <div id="home-page-service-article-2">
-            <h3>Service-2</h3>
-            <article id="home-page-service-article"></article>
-            <img src="" alt=''/>
-            <button>Read more</button>
-        </div>
-        <div id="home-page-service-article-3">
-            <h3>Service-3</h3>
-            <article id="home-page-service-article"></article>
-            <img src="" alt=''/>
-            <button>Read more</button>
-        </div>
+      <div id="home-page-service-article-1">
+        <h3>Service-1</h3>
+        <article id="home-page-service-article"></article>
+        <img src="" alt='' />
+        <button>Read more</button>
+      </div>
+      <div id="home-page-service-article-2">
+        <h3>Service-2</h3>
+        <article id="home-page-service-article"></article>
+        <img src="" alt='' />
+        <button>Read more</button>
+      </div>
+      <div id="home-page-service-article-3">
+        <h3>Service-3</h3>
+        <article id="home-page-service-article"></article>
+        <img src="" alt='' />
+        <button>Read more</button>
+      </div>
     </div>
   )
 }
 
-export default Service
+export default Services
