83366221a400ae92533300b970eb932d88f40e7f
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-07-10 16:35:44 +0530

    updated day4

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-import '../static_files/App.css';
-import Footer from './Home/Footer';
-=======
 import '../styles/App.css';
->>>>>>> 2e65033843949b44c7a8184cefdbf8ed6c57775a
 import Header from './Home/Header';
 import Carousel from './Home/Carousel';
 import Careers from './Home/Careers';
 import Insight from './Home/Insight';
 import Services from './Home/Services';
-<<<<<<< HEAD
-import ContactUs from './Home/ContactUs/ContactUs';
-=======
 import ContactUs from './Home/ContactUs';
 import Footer from './Home/Footer';
->>>>>>> 2e65033843949b44c7a8184cefdbf8ed6c57775a
 
 function App() {
   return (
@@ -24,10 +15,7 @@ function App() {
       <Careers />
       <Insight />
       <Services />
-<<<<<<< HEAD
       <Footer /> */}
-=======
->>>>>>> 2e65033843949b44c7a8184cefdbf8ed6c57775a
       <ContactUs />
     </div>
   );
