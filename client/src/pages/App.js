313e0d603390e5e338630ce56993a58f58f53a0b
Krishna-3 <pulipati.krishnavamsi@gmail.com>
2023-07-12 11:27:17 +0530

    marque added

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -4,12 +4,14 @@ import Header from './Home/Header';
 import Carousel from './Home/Carousel';
 import Careers from './Home/Careers';
 import Service from './Home/Services';
+import Partners from './Home/Partners';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Carousel />
+      <Partners />
       <Careers />
       <Service />
       <Footer />
---
+++ client/src/pages/Home/Partners.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import '../../styles/Partners.css'
+const Partners = () => {
+    return (
+        <div className='body'>
+            <div class="scroller">
+                <div class="scroll-track">
+                    <div class="scroll">
+                        <img id='wipro' src="../../../Wipro_Primary Logo_Color_RGB.svg" alt="" />
+                    </div>
+
+                    <div class="scroll">
+                        <img id='virtusa' src="../../../virtusa-corporation.webp" alt="" />
+                    </div>
+                    <div class="scroll">
+                        <img id='quess' src="../../../Quess_Logo.png" alt="" />
+                    </div>
+
+                    <div class="scroll">
+                        <img id='1point1' src="../../../1point1.jpg" alt="" />
+                    </div>
+                    {/* <div class="scroll">
+                        <img className='partner' src="../../../techno.jpeg"alt="" />
+                    </div> */}
+                </div>
+
+                <div class="scroll-track">
+                    <div class="scroll">
+                        <img id='wipro' src="../../../Wipro_Primary Logo_Color_RGB.svg" alt="" />
+                    </div>
+
+                    <div class="scroll">
+                        <img id='virtusa' src="../../../virtusa-corporation.webp" alt="" />
+                    </div>
+                    <div class="scroll">
+                        <img id='quess' src="../../../Quess_Logo.png" alt="" />
+                    </div>
+
+                    <div class="scroll">
+                        <img id='1point1' src="../../../1point1.jpg" alt="" />
+                    </div>
+                    {/* <div class="scroll">
+                        <img className='partner' src="../../../techno.jpeg"alt="" />
+                    </div> */}
+                </div>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Partners
