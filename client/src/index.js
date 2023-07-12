69dcb6d5d456d2966c2a97025f52785eaa8dbbd3
Anjani sai sudheer divyakolu <sudheerdivyakolu@gmail.com>
2023-07-12 17:04:45 +0530

    okie

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './App';
+import App from './pages/App';
 
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
