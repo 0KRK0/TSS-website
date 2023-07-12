f5b19c5490e4e85e6517d0bad113fd54d9297640
Anjani sai sudheer divyakolu <sudheerdivyakolu@gmail.com>
2023-07-12 12:41:50 +0530

    commited

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import '../styles/App.css';
+import Footer from './pages/Home/Footer';
+import Header from './pages/Home/Header';
+import Carousel from './pages/Home/Carousel';
+import Careers from './pages/Home/Careers';
+import Service from './pages/Home/Services';
+import Partners from './pages/Home/Partners';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Carousel />
+      <Partners />
+      <Careers />
+      <Service />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
