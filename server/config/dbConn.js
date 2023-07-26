e66ae703cd147d1efef8eab8e5e88025966c2527
0KRK0 <konarajeshkumar011@gmail.com>
2023-07-26 17:23:09 +0530

    v1

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://krishna:iGvOQcs6piQAeM46@cluster0.vzuxyaa.mongodb.net/?retryWrites=true&w=majority', {
+        await mongoose.connect('', {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
